3f9b32fa39e460b87d4b55e6975528d2800d7819
Surya Iyer <iyer.surya98@gmail.com>
2025-09-02 14:16:15 +0530

    feat: add youtube search

---
+++ server.py
@@ -0,0 +1,24 @@
+from youtube_search import YoutubeSearch
+from mcp.server.fastmcp import FastMCP
+import json
+
+mcp = FastMCP("youtube-search")
+
+
+@mcp.tool()
+def search_for_youtube_videos(query: str):
+    search = YoutubeSearch(search_terms=query, max_results=10)
+    return {
+        "videos": [
+            {
+                k: v
+                for k, v in video_obj.items()
+                if k not in ["thumbnails", "long_desc", "url_suffix"]
+            }
+            for video_obj in search.to_dict()
+        ]
+    }
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
